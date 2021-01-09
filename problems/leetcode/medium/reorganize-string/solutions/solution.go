00ef93221603fbd185b06ae99b8ac40075452f6c
Natechawin <natechawin@gmail.com>
2021-01-09 19:47:14 +0700

    Refactoring

--- problems/leetcode/medium/reorganize-string/solutions/solution.go
+++ problems/leetcode/medium/reorganize-string/solutions/solution.go
@@ -19,56 +19,45 @@ func reorganizeString(S string) string {
 	}
 
 	letterMapper := categorizeNumberOfEachRuneIn(runes)
-	letterInfoes := createLetterInStringInfosFrom(letterMapper)
-	sortedLetterInfoes := descSort(letterInfoes)
+	letterInfos := createLetterInStringInfosFrom(letterMapper)
+	sortedLetterInfos := descSort(letterInfos)
 
 	firstIdxPointer := 0
 	secondIdxPointer := 1
 
 	result := make([]rune, 0)
 
-	if len(sortedLetterInfoes) <= 1 {
+	if len(sortedLetterInfos) <= 1 {
 		return notPossibleCase
 	}
 	
-	for firstIdxPointer < len(sortedLetterInfoes) {
-		for sortedLetterInfoes[firstIdxPointer].NumberOfLetter > 0 && sortedLetterInfoes[secondIdxPointer].NumberOfLetter > 0 {
-			result = append(result, sortedLetterInfoes[firstIdxPointer].Letter)
-			result = append(result, sortedLetterInfoes[secondIdxPointer].Letter)
+	for firstIdxPointer < len(sortedLetterInfos) {
+		for haveLetterToInsertAt(firstIdxPointer, sortedLetterInfos) && haveLetterToInsertAt(secondIdxPointer, sortedLetterInfos) {
 
-			sortedLetterInfoes[firstIdxPointer].NumberOfLetter--
-			sortedLetterInfoes[secondIdxPointer].NumberOfLetter--
+			result = append(result, sortedLetterInfos[firstIdxPointer].Letter)
+			result = append(result, sortedLetterInfos[secondIdxPointer].Letter)
+
+			sortedLetterInfos[firstIdxPointer].NumberOfLetter--
+			sortedLetterInfos[secondIdxPointer].NumberOfLetter--
 		}
 
-		if sortedLetterInfoes[firstIdxPointer].NumberOfLetter <= 0 {
-			if newSelectedPointer, canSelectNewIndex := selectNextPointer(sortedLetterInfoes, firstIdxPointer, secondIdxPointer); canSelectNewIndex {
+		if sortedLetterInfos[firstIdxPointer].NumberOfLetter <= 0 {
+			newSelectedPointer, canSelectNextPointer := trySelectNextPointer(sortedLetterInfos, firstIdxPointer, secondIdxPointer)
+
+			if canSelectNextPointer {
 				firstIdxPointer = newSelectedPointer
 			} else {
-				if sortedLetterInfoes[secondIdxPointer].NumberOfLetter == 0 {
-					return string(result)
-				} else {
-					if result, canInsert := tryInsertLeftOverLettersTo(result, sortedLetterInfoes[secondIdxPointer]); canInsert {
-						return string(result)
-					} else {
-						return notPossibleCase
-					}
-				}
+				return generateResult(result, sortedLetterInfos, secondIdxPointer)
 			}
 		}
 
-		if sortedLetterInfoes[secondIdxPointer].NumberOfLetter <= 0 {
-			if newSelectedPointer, canSelectNewIndex := selectNextPointer(sortedLetterInfoes, secondIdxPointer, firstIdxPointer); canSelectNewIndex {
+		if sortedLetterInfos[secondIdxPointer].NumberOfLetter <= 0 {
+			newSelectedPointer, canSelectNextPointer := trySelectNextPointer(sortedLetterInfos, secondIdxPointer, firstIdxPointer)
+
+			if canSelectNextPointer {
 				secondIdxPointer = newSelectedPointer
 			} else {
-				if sortedLetterInfoes[firstIdxPointer].NumberOfLetter == 0 {
-					return string(result)
-				} else {
-					if result, canInsert := tryInsertLeftOverLettersTo(result, sortedLetterInfoes[firstIdxPointer]); canInsert {
-						return string(result)
-					} else {
-						return notPossibleCase
-					}
-				}
+				return generateResult(result, sortedLetterInfos, firstIdxPointer)
 			}
 		}
 	}
@@ -76,7 +65,11 @@ func reorganizeString(S string) string {
 	return string(runes)
 }
 
-func selectNextPointer(letterInfos []models.LetterInfo, indexToSelect int, anotherIdx int) (newSelectedIndex int, canSelectNewIndex bool) {
+func haveLetterToInsertAt(idxPointer int, letterInfos []models.LetterInfo) bool {
+	return letterInfos[idxPointer].NumberOfLetter > 0
+}
+
+func trySelectNextPointer(letterInfos []models.LetterInfo, indexToSelect int, anotherIdx int) (newSelectedIndex int, canSelectNewIndex bool) {
 	for letterInfos[indexToSelect].NumberOfLetter == 0 || indexToSelect == anotherIdx {
 		indexToSelect++
 
@@ -125,26 +118,38 @@ func descSort(letterInfos []models.LetterInfo) []models.LetterInfo {
 	return letterInfos
 }
 
+func generateResult(currentResult []rune, letterInfos []models.LetterInfo, pointerToLeftOverLetterInfo int) string {
+
+	if letterInfos[pointerToLeftOverLetterInfo].NumberOfLetter == 0 {
+		return string(currentResult)
+	} else {
+		if result, canInsert := tryInsertLeftOverLettersTo(currentResult, letterInfos[pointerToLeftOverLetterInfo]); canInsert {
+			return string(result)
+		} else {
+			return notPossibleCase
+		}
+	}
+}
+
 func tryInsertLeftOverLettersTo(runes []rune, letterInfo models.LetterInfo) (result []rune, canInsert bool) {
 	idxToInsert := 0
 
 	for letterInfo.NumberOfLetter > 0 && idxToInsert <= len(runes)  {
-		if idxToInsert == len(runes) {
+
+		switch idxToInsert {
+		case 0:
+			if runes[idxToInsert] != letterInfo.Letter {
+				runes = append([]rune{letterInfo.Letter}, runes...)
+
+				letterInfo.NumberOfLetter--
+			}
+		case len(runes):
 			runes = append(runes, letterInfo.Letter)
 			letterInfo.NumberOfLetter--
-			break
-		}
-
-		if runes[idxToInsert] != letterInfo.Letter {
-			if idxToInsert > 0 {
-				if runes[idxToInsert - 1] != letterInfo.Letter {
-					runes = append(runes[:idxToInsert + 1], runes[idxToInsert:]...)
-					runes[idxToInsert] = letterInfo.Letter
-
-					letterInfo.NumberOfLetter--
-				}
-			} else {
-				runes = append([]rune{letterInfo.Letter}, runes...)
+		default:
+			if runes[idxToInsert] != letterInfo.Letter && runes[idxToInsert - 1] != letterInfo.Letter {
+				runes = append(runes[:idxToInsert + 1], runes[idxToInsert:]...)
+				runes[idxToInsert] = letterInfo.Letter
 
 				letterInfo.NumberOfLetter--
 			}
