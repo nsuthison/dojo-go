d6aacb9d9b694f3a6fdb49cffb4240f73a0edd59
Natechawin <natechawin@gmail.com>
2021-01-12 21:58:16 +0700

    add word comparing function

--- problems/leetcode/easy/verifying-an-alien-dictionary/solutions/solution.go
+++ problems/leetcode/easy/verifying-an-alien-dictionary/solutions/solution.go
@@ -1,7 +1,40 @@
 package solutions
 
 func isAlienSorted(words []string, order string) bool {
+	//letterOrderMapping := createLetterOrderMappingFrom(order)
+	//
+	//for i := 0; i < len(words); i++ {
+	//
+	//}
+
 	return false
+}
+
+func isLeftWordLesserThanRight(leftWord string, rightWord string, orderRule map[rune]int) bool {
+
+	leftWordAsRunes := []rune(leftWord)
+	rightWordAsRunes := []rune(rightWord)
+
+	for i := 0; i < len(leftWordAsRunes); i++ {
+
+		if i >= len(rightWordAsRunes) {
+			return false
+		}
+
+		leftLetterOrderPoint := orderRule[leftWordAsRunes[i]]
+		rightLetterOrderPoint := orderRule[rightWordAsRunes[i]]
+
+		switch {
+		case leftLetterOrderPoint < rightLetterOrderPoint:
+			return true
+		case leftLetterOrderPoint > rightLetterOrderPoint:
+			return false
+		default:
+			continue
+		}
+	}
+	
+	return true
 }
 
 func createLetterOrderMappingFrom(order string) map[rune]int {
--- problems/leetcode/easy/verifying-an-alien-dictionary/solutions/solution_test.go
+++ problems/leetcode/easy/verifying-an-alien-dictionary/solutions/solution_test.go
@@ -5,8 +5,19 @@ import (
 	"testing"
 )
 
+var isLeftWordLesserThanRightTestCases = []struct {
+	leftWord       string
+	rightWord      string
+	orderRule      map[rune]int
+	expectedResult bool
+}{
+	{"hello", "leetcode", map[rune]int{'h': 0, 'l': 1}, true},
+	{"word", "world", map[rune]int{'w': 0, 'o': 1, 'r': 2, 'l': 3, 'd': 4}, false},
+	{"apple", "app", map[rune]int{'a': 0, 'p': 1, 'l': 2, 'e': 3}, false},
+}
+
 func Test_createLetterOrderMappingFrom(t *testing.T) {
-	Convey("Given string contain letters which represent in order",t , func() {
+	Convey("Given string contain letters which represent in order", t, func() {
 
 		input := "bcgha"
 		Convey("When create letter order mapping", func() {
@@ -21,4 +32,24 @@ func Test_createLetterOrderMappingFrom(t *testing.T) {
 			})
 		})
 	})
+}
+
+func Test_isLeftWordLesserThanRight(t *testing.T) {
+	for _, testCaseData := range isLeftWordLesserThanRightTestCases {
+		t.Run("reorganizeStringSuccessCase", func(t *testing.T) {
+			Convey("Given some random string", t, func() {
+				leftWord := testCaseData.leftWord
+				rightWord := testCaseData.rightWord
+				letterOrderRule := testCaseData.orderRule
+
+				Convey("When pass the random string to reorganizeString function", func() {
+					isLeftWordLesserThanRight := isLeftWordLesserThanRight(leftWord, rightWord, letterOrderRule)
+
+					Convey("Then the result string shouldn't have same Letter adjacent to each other", func() {
+						So(isLeftWordLesserThanRight, ShouldEqual, testCaseData.expectedResult)
+					})
+				})
+			})
+		})
+	}
 }
