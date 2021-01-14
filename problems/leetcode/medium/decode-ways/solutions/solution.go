ec3ac2d0ffde5f9b65a36e72573e1efc5b4a88fe
Natechawin <natechawin@gmail.com>
2021-01-14 19:23:42 +0700

    Finish not so good solution because it'll consume a lot of memory

--- problems/leetcode/medium/decode-ways/solutions/solution.go
+++ problems/leetcode/medium/decode-ways/solutions/solution.go
@@ -1,12 +1,68 @@
 package solutions
 
+import "strconv"
+
 func numDecodings(s string) int {
-	return 0
+
+	tails := make([]int, 0)
+	hasNumber := false
+	isLegit := true
+
+	for _, runeDigit := range s {
+
+		digit, _ := strconv.Atoi(string(runeDigit))
+
+		if !hasNumber && digit == 0 {
+			continue
+		}
+
+		hasNumber = true
+
+		tails, isLegit = nextTails(tails, digit)
+		if !isLegit {
+			return 0
+		}
+	}
+
+	return len(tails)
 }
 
-//func calculateTail(tail []int, current) []int {
-//
-//}
+func nextTails(tails []int, toAppend int) (result []int, isAbleToGet bool) {
+
+	result = make([]int, 0)
+
+	if len(tails) == 0 {
+		return append(result, toAppend), true
+	}
+
+	if toAppend == 0 {
+		for _, tail := range tails {
+			if !isBeAbleToCombine(tail, 0) {
+				continue
+			} else {
+				combine := (tail * 10) + toAppend
+				result = append(result, combine)
+			}
+		}
+	} else {
+
+		for _, tail := range tails {
+			result = append(result, toAppend)
+
+			if isBeAbleToCombine(tail, toAppend) {
+				combine := (tail * 10) + toAppend
+
+				result = append(result, combine)
+			}
+		}
+	}
+
+	if len(result) == 0 {
+		return nil, false
+	} else {
+		return result, true
+	}
+}
 
 func isBeAbleToCombine(head int, tail int) bool {
 
--- problems/leetcode/medium/decode-ways/solutions/solution_test.go
+++ problems/leetcode/medium/decode-ways/solutions/solution_test.go
@@ -5,6 +5,40 @@ import (
 	"testing"
 )
 
+var numDecodingsTestCases = []struct {
+	input          string
+	expectedResult int
+}{
+	{"12",2},
+	{"226",3},
+	{"0", 0},
+	{"2101", 1},
+	{"10011", 0},
+}
+
+func Test_smt(t *testing.T) {
+	result := numDecodings("111111111111111111111111111111111111111111111")
+	So(result, ShouldEqual, 10)
+}
+
+func Test_numDecodings(t *testing.T) {
+	for _, testCase := range numDecodingsTestCases {
+		t.Run("numDecodingsTestCases", func(t *testing.T) {
+			Convey("Given input string contain only digits", t, func() {
+				input := testCase.input
+
+				Convey("When pass the random string to reorganizeString function", func() {
+					result := numDecodings(input)
+
+					Convey("Then the result string shouldn't have same Letter adjacent to each other", func() {
+						So(result, ShouldEqual, testCase.expectedResult)
+					})
+				})
+			})
+		})
+	}
+}
+
 var isAbleToCombineTestCases = []struct {
 	head           int
 	tail           int
@@ -20,7 +54,7 @@ var isAbleToCombineTestCases = []struct {
 
 func Test_isBeAbleToCombine(t *testing.T) {
 	for _, testCase := range isAbleToCombineTestCases {
-		t.Run("reorganizeStringSuccessCase", func(t *testing.T) {
+		t.Run("isAbleToCombineTestCases", func(t *testing.T) {
 			Convey("Given head and tail as int to combine", t, func() {
 				head := testCase.head
 				tail := testCase.tail
