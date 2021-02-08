8fccac2d724bf0b91aaa2e1c44a966f1d44a3b0d
Natechawin <natechawin@gmail.com>
2021-02-08 14:52:06 +0700

    Fix error

--- problems/leetcode/hard/text-justification/solutions/solution.go
+++ problems/leetcode/hard/text-justification/solutions/solution.go
@@ -1,24 +1,33 @@
 package solutions
 
+// Question: https://leetcode.com/problems/text-justification/
 func fullJustify(words []string, maxWidth int) []string {
 	return make([]string, 0)
 }
 
-func groupTextForEachLine(words []string, maxWidth int) [][]string {
-	toReturn := make([][]string, 0)
+// func groupTextForEachLine(words []string, maxWidth int) [][]string {
+// 	toReturn := make([][]string, 0)
 
-	eachLine := make([]string, 0)
-	for _, word := range words {
-		totalLen(eachLine)
-	}
-}
+// 	eachLine := make([]string, 0)
+// 	for _, word := range words {
+// 		totalLen(eachLine)
+// 	}
+// }
 
-func totalLen(words []string) int {
+func totalLenWithSpace(words []string) int {
 	totalLen := 0
 
 	for _, word := range words {
 		totalLen += len(word)
 	}
 
-	return totalLen
+	return totalLen + totalSpaceFor(len(words))
+}
+
+func totalSpaceFor(numberOfWord int) int {
+	if numberOfWord <= 1 {
+		return 0
+	}
+
+	return numberOfWord - 1
 }
